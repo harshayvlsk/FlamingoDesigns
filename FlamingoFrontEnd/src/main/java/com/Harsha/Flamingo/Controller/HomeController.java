aedc38aa322e5bd98f297b8e45ace90e43147622
harshayvlsk <harshayvlsk@gmail.com>
2016-10-24 21:14:46 +0530

    This is my first commit

---
+++ FlamingoFrontEnd/src/main/java/com/Harsha/Flamingo/Controller/HomeController.java
@@ -0,0 +1,43 @@
+package com.Harsha.Flamingo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+
+public class HomeController {
+	@RequestMapping("/")
+	public String gotohome()
+	{
+		return "index";
+	}
+	@RequestMapping("/validate")
+	public String validate(@RequestParam(name="userID") String id, @RequestParam("password")String pwd,Model model)
+	{
+		if(id.equals("niit") && pwd.equals("niit"))
+			{
+				model.addAttribute("Success","Logged in");
+				return "index";
+			}
+		else
+			{
+			model.addAttribute("Error","Please Check");
+			return "index";
+			}
+	}
+	
+	@RequestMapping("/Login")
+	public String login(Model model)
+	{
+		model.addAttribute("userClickedLogin","true");
+		return "login";
+	}
+	@RequestMapping("/Register")
+	public String register(Model model)
+	{
+		model.addAttribute("userClickedRegister","true");
+		return "register";
+	}
+}
---
+++ FlamingoFrontEnd/src/main/webapp/WEB-INF/Views/index.jsp
@@ -0,0 +1,33 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix="c" 
+           uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Welcome to Flamingo</title>
+</head>
+
+<body>
+<h2><center> Flamingo Designs </center></h2>
+Existing User 
+<a href="Login">login here</a> New User:
+<a href="Register">Register here</a>
+
+<hr>
+
+<c:if test="$(userClickedLogin)">
+<jsp:include page="login.jsp"></jsp:include>
+</c:if>
+
+<c:if test="$(userClickedRegister)">
+<jsp:include page="register.jsp"></jsp:include>
+</c:if>
+
+<c:out value="${Success}"/>
+<c:out value="${Error}"/>
+
+</body>
+</html>
